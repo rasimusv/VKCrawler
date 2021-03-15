12b4716aad3dfdb9c168da953bb9643b56939282
rasimusv <rasimusv@gmail.com>
2021-03-15 06:09:38 +0300

    Optimized code

--- main.py
+++ main.py
@@ -14,17 +14,15 @@ TOKEN = '45eec18445eec18445eec184c745985afd445ee45eec18425d9183af519b65585e20db0
 
 
 def get_wall(group_urn):
+    session = vk.Session(access_token=TOKEN)
+    vk_api = vk.API(session)
     first = vk_api.wall.get(domain=group_urn, count=100, v=5.137)
     posts = first["items"]
     posts = posts + vk_api.wall.get(domain=group_urn, count=100, offset=100, v=5.137)["items"]
     return posts
 
 
-if __name__ == "__main__":
-    session = vk.Session(access_token=TOKEN)
-    vk_api = vk.API(session)
-    wall = get_wall(GROUP_URN)
-
+def get_dictionary(wall):
     tt = str.maketrans(dict.fromkeys(string.punctuation))
 
     dictionary = {}
@@ -37,7 +35,10 @@ if __name__ == "__main__":
                 dictionary[word] += 1
             else:
                 dictionary[word] = 1
+    return dictionary
 
+
+def save_data(dictionary):
     table_name = "data" + str(now.strftime("%d%m%Y%H%M"))
 
     cursor.execute("CREATE TABLE " + table_name + " (id bigserial, word varchar, count bigint)")
@@ -48,3 +49,15 @@ if __name__ == "__main__":
         cursor.execute(insert)
     connection.commit()
     connection.close()
+    return
+
+
+def run_script():
+    wall = get_wall(GROUP_URN)
+    dictionary = get_dictionary(wall)
+    save_data(dictionary)
+    return
+
+
+if __name__ == "__main__":
+    run_script()
